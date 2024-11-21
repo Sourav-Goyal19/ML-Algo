7a86f47e3a9fda91d90d491321d1065ac94e4754
Sourav-Goyal19 <goyalsourav935@gmail.com>
2024-11-21 22:49:14 +0530

    Added Authentication

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,10 +1,26 @@
+import { Button } from "@/components/ui/button";
 import AuthForm from "./components/auth-form";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 
 const SignUpPage = () => {
   return (
-    <div className="bg-background min-h-screen flex flex-col w-full items-center justify-center">
-      <h1 className="text-3xl font-bold text-center">Create Your Account</h1>
+    <div className="bg-gradient-to-b from-black via-gray-900 to-gray-900 min-h-screen flex flex-col w-full items-center justify-center">
+      <h1 className="text-3xl font-bold text-center text-white">
+        Create Your Account
+      </h1>
       <AuthForm />
+      <div className="mt-8 text-center">
+        <Button
+          variant="outline"
+          className="bg-white/5 text-blue-300 border-blue-500/30 hover:bg-blue-500/20 hover:text-blue-300 hover:opacity-80 transition-all duration-300 py-5 px-8 rounded-md shadow-lg"
+        >
+          <Link href="/" className="w-full flex">
+            <ArrowLeft size={20} />
+            <span className="ml-2">Back to Home</span>
+          </Link>
+        </Button>
+      </div>
     </div>
   );
 };
